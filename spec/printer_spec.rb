98233e21d75b87b30643a0878572de7aebc4cafd
ollieh-m <ollie.hm@cantab.net>
2016-06-28 15:14:54 +0100

    Test printer adds values from each hash to a new line

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -5,14 +5,17 @@ describe Printer do
 	let(:array){ [{date: DateTime.new(2001,2,3), credit: 5, debit: 0, balance: 10}] }
 	subject(:printer){ described_class.new }
 
+	let(:printout){ printer.printout(array) }
+
 	context '#print' do
+
 		it 'uses keys from the first hash in an array as headers' do
-			printout = printer.printout(array)
 			match = printout =~ /date \|\| credit \|\| debit \|\| balance/
 			expect(match).to eq 0
 		end
-		it 'adds the values in each hash to the printout on new lines' do
-			
+		it 'adds the values from each hash to a new line on the printout' do
+			row = printout.split("\n")[1]
+			expect(row).to eq '02-03-2001 || 5.00 || || 10.00'
 		end
 	end
 
