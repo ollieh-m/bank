be3ead8787e9ae75e96e050f291baee8d14fb9e2
ollieh-m <ollie.hm@cantab.net>
2016-06-28 11:58:08 +0100

    Pass test

--- lib/deposit.rb
+++ lib/deposit.rb
@@ -2,10 +2,11 @@ require 'date'
 
 class Deposit
 	
-	attr_reader :date
+	attr_reader :date, :amount
 
-	def initialize
+	def initialize(amount:)
 		@date = Date.today
+		@amount = amount
 	end
 
 end
