7576d8e9d555ebddb8703082581ed3877ee9cf53
ollieh-m <ollie.hm@cantab.net>
2016-06-28 14:59:13 +0100

    refactor printer

--- lib/printer.rb
+++ lib/printer.rb
@@ -4,12 +4,22 @@ class Printer
 	end
 
 	def printout(array)
+		header = header(array.first)
+
 		header = ""
 		array.first.each_key do |key|
 			header << "#{key} "
 		end
 		header = header.gsub(/(\w)( )(\w)/){"#{$1} || #{$3}"}
 		header
+	end
+
+	private
+
+	def header(hash)
+		header = ""
+		hash.each_key{ |key| header << "#{key} " }
+		header = header.gsub(/(\w)( )(\w)/){"#{$1} || #{$3}"}
 	end
 
 end
