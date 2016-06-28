d2f439d8e6589690e34f211b5a707ba32b701025
ollieh-m <ollie.hm@cantab.net>
2016-06-28 12:09:31 +0100

    Tests for withdrawal - same as deposit but amount is stored as a negative

---
+++ spec/withdrawal_spec.rb
@@ -0,0 +1,28 @@
+require 'withdrawal'
+
+describe Withdrawal do
+
+	subject(:withdrawal_one){ described_class.new(amount: 1) }
+	subject(:withdrawal_two){ described_class.new(amount: 2) }
+
+	before do
+		allow(DateTime).to receive(:now).and_return(:this_moment)
+	end
+
+	context 'On initialization' do
+
+		it 'stores the current date and time' do
+			expect(withdrawal_one.date).to eq :this_moment
+		end
+
+		it 'stores the amount passed in' do
+			expect(withdrawal_one.amount).to eq -1
+		end
+
+		it 'stores a different amount passed in' do
+			expect(withdrawal_two.amount).to eq -2
+		end
+
+	end
+
+end
