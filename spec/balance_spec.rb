1d17495e4791033f020bd37f88566866c036761a
ollieh-m <ollie.hm@cantab.net>
2016-06-28 11:14:27 +0100

    Test updating balance with positive amount

--- spec/balance_spec.rb
+++ spec/balance_spec.rb
@@ -5,11 +5,16 @@ describe Balance do
 	subject(:balance){ described_class.new }
 
 	context 'On initialization' do
-		
 		it 'Has no funds' do
 			expect(balance.now).to eq 0
 		end
+	end
 
+	context ' #update' do
+		it 'adds the amount passed in if the amount is positive' do
+			balance.update(1)
+			expect(balance.now).to eq 1
+		end
 	end
 
 end
