e5c82a4df5b8892e185aa03b848f79219e18c804
ollieh-m <ollie.hm@cantab.net>
2016-06-28 13:29:14 +0100

    New account test - transaction instructs log to store details

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,9 @@
 require_relative 'balance.rb'
+require_relative 'log.rb'
 
 class Account
 
-	def initialize(balance = Balance.new)
+	def initialize(balance = Balance.new, log = Log.new)
 		@balance = balance
 	end
 
