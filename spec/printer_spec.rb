13e27c7244657270de82bd409e6b58da930ec390
ollieh-m <ollie.hm@cantab.net>
2016-06-28 14:12:11 +0100

    First test for printer - extracting header from first hash in array

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,13 +1,16 @@
-# require 'printer'
+require 'printer'
 
-# describe Printer do
+describe Printer do
 
-# 	let(:array){ {date: DateTime.new(2001,2,3), } }
+	let(:array){ [{date: DateTime.new(2001,2,3), credit: 5, debit: 0, balance: 10}] }
+	subject(:printer){ described_class.new }
 
-# 	context '#print' do
-# 		it 'uses keys from the first hash in an array as headers' do
+	context '#print' do
+		it 'uses keys from the first hash in an array as headers' do
+			printout = printer.printout(array)
+			match = printout =~ /date || credit || debit || balance/
+			expect(match).to eq 0
+		end
+	end
 
-# 		end
-# 	end
-
-# end
+end
