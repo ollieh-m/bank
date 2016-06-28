5638af0c0692b53389cb267cacd0a81d6614d4da
ollieh-m <ollie.hm@cantab.net>
2016-06-28 13:10:04 +0100

    Adjust test so store method takes the transaction type as well - test for withdrawals as well as deposits

--- spec/log_spec.rb
+++ spec/log_spec.rb
@@ -2,17 +2,25 @@ require 'log'
 
 describe Log do
 
-	let(:transaction_date){ double(:deposit_date) }
+	let(:transaction_date){ double(:transaction_date) }
 	let(:transaction_amount){ 5 }
 	let(:balance_now){ 10 }
 	subject(:log){ described_class.new }
 
 	context '#store' do
 		
-		it 'adds a hash to the logged array with all the details passed in' do
-			log.store(transaction_date,transaction_amount,balance_now)
+		it 'adds a hash to the logged array with all the deposit details passed in' do
+			transaction_type = :credit
+			log.store(transaction_date,transaction_type,transaction_amount,balance_now)
 			expect(log.show).to eq [{date: transaction_date, credit: true, debit: false, amount: transaction_amount, balance: balance_now}]
 		end
+
+		it 'adds a hash to the logged array with all the withdrawal details passed in' do
+			transaction_type = :debit
+			log.store(transaction_date,transaction_type,transaction_amount,balance_now)
+			expect(log.show).to eq [{date: transaction_date, credit: false, debit: true, amount: transaction_amount, balance: balance_now}]
+		end
+
 	end
 
 end
